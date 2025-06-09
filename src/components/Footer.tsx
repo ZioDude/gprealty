43f819006d73dd8cbd33d4997444599ac1fd6ced
ZioDude <akyriakouu@gmail.com>
2025-06-09 23:23:24 +0300

    Remove unused Link import from Footer component - final TypeScript fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { Facebook, Twitter, Instagram, Linkedin } from 'lucide-react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
